568fc2b765a653d0731fcbb5d7b345893246f03d
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2024-07-18 07:22:42 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # requests_oauth2
+Login OAuth2
+
+# SECURITY NOTE:
+I wrote the .py files.  You have my word that they don't do anything nefarious.  Even so, I recommend that you perform
+your own static analysis and supply chain testing before use.  Many libraries are imported that are not in my own control.
+
+# usage
+```
+$ python github_login.py -h
+usage: github_login.py [-h] --client_id CLIENT_ID --client_secret CLIENT_SECRET --target_url TARGET_URL [--proxies PROXIES]
+
+options:
+  -h, --help                        show this help message and exit
+  --client_id CLIENT_ID             The user name for your IdP account
+  --client_secret CLIENT_SECRET     The password for your IdP account
+  --target_url TARGET_URL           Target URL after login
+  --proxies PROXIES                 JSON structure specifying 'http' and 'https' proxy URLs
+```
+
+# example
+```
+$ python github_logon.py --sp_url https://app.example.com/sso/saml/start --username MyUsername --password MyPassword
+
+```
---
+++ github_login.py
@@ -0,0 +1,72 @@
+import argparse
+
+from json import loads
+from typing import Optional
+
+from requests import Response
+from requests_oauthlib import OAuth2Session
+
+USERNAME_KEYS: list = ['username', 'user_name', 'client_id']
+PASSWORD_KEYS: list = ['password', 'passwd', 'client_secret']
+MAX_REDIRECTS: int = 10
+
+
+def main(
+        client_id: str,
+        client_secret: str,
+        target_url: Optional[str] = None,
+        proxies: Optional[dict] = None,
+) -> Response:
+    session = OAuth2Session(client_id=client_id)
+    session.proxies = proxies
+    authorization_base_url = 'https://github.com/login/oauth/authorize'
+    token_url = 'https://github.com/login/oauth/access_token'
+    authorization_url, state = session.authorization_url(authorization_base_url)
+    print(f'Authorize here: {authorization_url}')
+    redirect_response = input('Redirect URL: ')
+    session.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
+    return session.get(target_url)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--client_id',
+        type=str,
+        required=True,
+        help='The user name for your IdP account',
+    )
+    parser.add_argument(
+        '--client_secret',
+        type=str,
+        required=True,
+        help='The password for your IdP account',
+    )
+    parser.add_argument(
+        '--target_url',
+        type=str,
+        required=True,
+        help='Target URL after login',
+    )
+    parser.add_argument(
+        '--proxies',
+        type=str,
+        required=False,
+        help="JSON structure specifying 'http' and 'https' proxy URLs",
+    )
+
+    args = parser.parse_args()
+
+    proxies: Optional[dict] = None
+    if args.proxies:
+        proxies: dict = loads(args.proxies)
+
+    if data := main(
+        client_id=args.client_id,
+        client_secret=args.client_secret,
+        target_url=args.target_url,
+        proxies=proxies,
+    ):
+        print(data)
+    else:
+        print('whoops...')
